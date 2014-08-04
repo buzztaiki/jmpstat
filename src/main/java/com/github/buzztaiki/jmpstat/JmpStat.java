c11dcb43c96eac7d39ad6b078f8677356c38045b
Taiki Sugawara <buzz.taiki@gmail.com>
2014-08-05 02:56:50 +0900

    refactor

--- src/main/java/com/github/buzztaiki/jmpstat/JmpStat.java
+++ src/main/java/com/github/buzztaiki/jmpstat/JmpStat.java
@@ -14,51 +14,29 @@ import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 
 public class JmpStat {
-    private void usage(PrintStream out) {
-        out.println("Usage: jmpstat <host>:<port> [<pool_names> <interval>]");
+    private final MBeanServerConnection server;
+    private final PrintStream out;
+
+    public JmpStat(MBeanServerConnection server, PrintStream out) {
+        this.server = server;
+        this.out = out;
     }
 
-    private int run(Args args) throws Exception {
-        String conn = args.get(0);
-        if (conn == null) {
-            usage(System.err);
-            return 1;
-        }
-        try (JMXConnector jmxConn = JMXConnectorFactory.connect(getUrl(conn))) {
-            MBeanServerConnection server = jmxConn.getMBeanServerConnection();
-            Set<String> poolNames = poolNames(args.get(1, ""));
-            System.out.println(poolNames.size());
-            if (poolNames.isEmpty()) {
-                printAllPools(server);
-            } else {
-                long interval = Long.parseLong(args.get(2, "1000"));
-                pollLoop(server, poolNames, interval);
-            }
-            return 0;
+    public void printAllPools() throws IOException {
+        for (MemoryPoolMXBean memoryPool : getMemoryPools()) {
+            out.format("%s\t%s%n", memoryPool.getName(), memoryPool.getUsage());
         }
     }
 
-    private Set<String> poolNames(String arg) {
-        Set<String> xs = new HashSet<>(Arrays.asList(arg.split(" *, *", 0)));
-        xs.remove("");
-        return xs;
-    }
-
-    private void printAllPools(MBeanServerConnection server) throws IOException {
-        for (MemoryPoolMXBean memoryPool : ManagementFactory.getPlatformMXBeans(server, MemoryPoolMXBean.class)) {
-            System.out.format("%s\t%s%n", memoryPool.getName(), memoryPool.getUsage());
-        }
-    }
-
-    private void pollLoop(MBeanServerConnection server, Set<String> poolNames, long interval) throws IOException {
-        System.out.println("pool_name\tinit\tused\tcommitted\tmax");
+    public void pollLoop(Set<String> poolNames, long interval) throws IOException {
+        out.println("pool_name\tinit\tused\tcommitted\tmax");
         for (;;) {
-            for (MemoryPoolMXBean memoryPool : ManagementFactory.getPlatformMXBeans(server, MemoryPoolMXBean.class)) {
+            for (MemoryPoolMXBean memoryPool : getMemoryPools()) {
                 if (!poolNames.contains(memoryPool.getName())) {
                     continue;
                 }
                 MemoryUsage usage = memoryPool.getUsage();
-                System.out.format("%s\t%d\t%d\t%d\t%d%n",
+                out.format("%s\t%d\t%d\t%d\t%d%n",
                     memoryPool.getName(),
                     usage.getInit()/1024,
                     usage.getUsed()/1024,
@@ -73,30 +51,7 @@ public class JmpStat {
         }
     }
 
-    private JMXServiceURL getUrl(String conn) throws IOException {
-        try {
-            return getPidUrl(Integer.parseInt(conn));
-        } catch (NumberFormatException e) {
-            return new JMXServiceURL("service:jmx:rmi:///jndi/rmi://" + conn + "/jmxrmi");
-        }
-    }
-
-    private JMXServiceURL getPidUrl(int pid) throws IOException {
-/*
-        LocalVirtualMachine vm = LocalVirtualMachine.getLocalVirtualMachine(pid);
-        if (vm.isManageable()) {
-            return new JMXServiceURL(vm.toUrl());
-        }
-        vm.startManagementAgent();
-        if (vm.isManageable()) {
-            return new JMXServiceURL(vm.toUrl());
-        }
-        throw new IOException("Could not start agent for " + pid);
-*/
-        throw new UnsupportedOperationException("getPidUrl");
-    }
-
-    public static void main(String[] args) throws Exception {
-        System.exit(new JmpStat().run(Args.of(args)));
+    private Iterable<MemoryPoolMXBean> getMemoryPools() throws IOException {
+        return ManagementFactory.getPlatformMXBeans(server, MemoryPoolMXBean.class);
     }
 }
---
+++ src/main/java/com/github/buzztaiki/jmpstat/Main.java
@@ -0,0 +1,69 @@
+package com.github.buzztaiki.jmpstat;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import javax.management.MBeanServerConnection;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+
+public class Main {
+    private void usage(PrintStream out) {
+        out.println("Usage: jmpstat <host>:<port> [<pool_names> <interval>]");
+    }
+
+    private int run(Args args) throws Exception {
+        String conn = args.get(0);
+        if (conn == null) {
+            usage(System.err);
+            return 1;
+        }
+        try (JMXConnector jmxConn = JMXConnectorFactory.connect(getUrl(conn))) {
+            JmpStat jmpStat = new JmpStat(jmxConn.getMBeanServerConnection(), System.out);
+            Set<String> poolNames = poolNames(args.get(1, ""));
+            if (poolNames.isEmpty()) {
+                jmpStat.printAllPools();
+            } else {
+                long interval = Long.parseLong(args.get(2, "1000"));
+                jmpStat.pollLoop(poolNames, interval);
+            }
+            return 0;
+        }
+    }
+
+    private Set<String> poolNames(String arg) {
+        Set<String> xs = new HashSet<>(Arrays.asList(arg.split(" *, *", 0)));
+        xs.remove("");
+        return xs;
+    }
+
+    private JMXServiceURL getUrl(String conn) throws IOException {
+        try {
+            return getPidUrl(Integer.parseInt(conn));
+        } catch (NumberFormatException e) {
+            return new JMXServiceURL("service:jmx:rmi:///jndi/rmi://" + conn + "/jmxrmi");
+        }
+    }
+
+    private JMXServiceURL getPidUrl(int pid) throws IOException {
+/*
+        LocalVirtualMachine vm = LocalVirtualMachine.getLocalVirtualMachine(pid);
+        if (vm.isManageable()) {
+            return new JMXServiceURL(vm.toUrl());
+        }
+        vm.startManagementAgent();
+        if (vm.isManageable()) {
+            return new JMXServiceURL(vm.toUrl());
+        }
+        throw new IOException("Could not start agent for " + pid);
+*/
+        throw new UnsupportedOperationException("getPidUrl");
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.exit(new Main().run(Args.of(args)));
+    }
+}
